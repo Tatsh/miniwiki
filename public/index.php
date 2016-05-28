79dd65ffbdc788498727450d6bdc261ed3d61642
Andrew Udvare <audvare@gmail.com>
2016-05-28 06:42:20 -0700

    Use Miniwiki App class

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ session_start();
 
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
+$app = new \Miniwiki\App($settings);
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
